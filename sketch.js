c84b0f6a69775eca8b79d9a79849b520f12bdfaa
Lorahmorcega <00001099316376sp@al.educacao.sp.gov.br>
2024-08-05 17:03:00 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(1000, 800);
+}
+background("rgb(241,164,213)")
+function draw() {
+  
+  circle (mouseX,mouseY,50)
+  fill("black")
+if(mouseIsPressed)
+circle (mouseX,mouseY,50)
+}
